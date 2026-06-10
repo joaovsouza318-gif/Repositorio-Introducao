71cb21319b67b00d3bdaf30d403cd0323d4ed502
Joao Tabosa <joao.v.souza318@aluno.senaibr>
2026-06-09 21:46:53 -0300

    JS

--- js/script.js
+++ js/script.js
@@ -1,7 +1,14 @@
-const formPessoa = document.querySelector
-("#formulario-pessoa")
+const formPessoa = document.querySelector("#formulario-pessoa")
+const divDados = document.querySelector("#div-dados")
 
-const divDados = document.querySelector
-("#div-dados")
-document.getElementById = 
-    if formPessoa.idade >=
+formPessoa.addEventListener('submit',(evt)=>{
+    evt.preventDefault()
+    const dadosForm = new FormData(formPessoa)
+
+    const nome = dadosForm.get('nome')
+    const idade = dadosForm.get('idade')
+
+let situacaoIdade = idade >= 18 ? `${nome}, é Maior de idade`:`${nome}, é Menor de idade`
+divDados.innerHTML = situacaoIdade
+
+})
