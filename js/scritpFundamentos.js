6b51857cbc4701852e9b1385f7efc17360457ef7
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-10 20:03:40 -0300

    Introdução ao JS

---
+++ js/scritpFundamentos.js
@@ -0,0 +1,26 @@
+//comentário de uma linha  
+/*
+comentário de múltiplas linhas
+*/
+
+let num = 10
+var num2 = 20
+const num3 = 30
+
+console.log(num)
+console.log(num2)
+console.log(num3)
+
+if (true) {
+    let num4 = 40
+    console.log(num4)
+
+    var num5 = 50
+}
+console.log(num5)
+num = 60
+console.log(num)
+num2 = 70
+console.log(num2)
+num3 = 80
+console.log(num3)
