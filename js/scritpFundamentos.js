4c9435611da590bf086ee767a53eec229251ca33
Noite Cetafaju <Noite.Cetafaju@labfies.org.br>
2026-06-10 21:05:40 -0300

    Inserções de Texto

--- js/scritpFundamentos.js
+++ js/scritpFundamentos.js
@@ -17,10 +17,10 @@ if (true) {
 
     var num5 = 50
 }
-console.log(num5)
-num = 60
-console.log(num)
-num2 = 70
-console.log(num2)
-num3 = 80
-console.log(num3)
+console.log("Olá, mundo!",num2)
+
+console.log("A soma de num e num2 é: " + (num + num2))
+console.log("A subtração de num e num2 é: " + (num - num2))
+console.log("A multiplicação de num e num2 é: " + (num * num2))
+console.log("A divisão de num e num2 é: " + (num / num2))
+console.log("O módulo de num e num2 é: " + (num % num2))
